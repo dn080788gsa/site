4e5552292761601daf87b9ad1db4b44833554aba
dn080788gsa <37812237+dn080788gsa@users.noreply.github.com>
2019-03-20 17:20:18 +0200

    Add files via upload

---
+++ precache-manifest.bf7fc5f3f1c3bffc9f7ba34687ef3790.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "67ec2aaf186f732a81b22df4d2350efb",
+    "url": "/static/media/logo.67ec2aaf.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2b1ef3ca94d69d183d1",
+    "url": "/static/js/main.c2b1ef3c.chunk.js"
+  },
+  {
+    "revision": "cc433f2a7e15c687bfb0",
+    "url": "/static/js/1.cc433f2a.chunk.js"
+  },
+  {
+    "revision": "c2b1ef3ca94d69d183d1",
+    "url": "/static/css/main.052005be.chunk.css"
+  },
+  {
+    "revision": "5e26b5cd286e860fa01157ac3d33633e",
+    "url": "/index.html"
+  }
+];
